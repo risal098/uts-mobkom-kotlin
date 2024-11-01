932f6ce102034b54d3c66f593defb17dcdb3835b
risal098 <yesitsme811@gmail.com>
2024-11-01 19:28:57 +0700

    Changing permission of gradlew

---
+++ app/src/main/java/com/example/utsmobcom/type/functionality.kt
@@ -0,0 +1,35 @@
+package com.example.utsmobcom.type
+
+import androidx.navigation.findNavController
+import com.example.utsmobcom.type.Book
+
+
+// navigate to add page 
+fun goToSkipThisPage(){
+findNavController().navigate(
+    R.id.page2Add, 
+    null, 
+    NavOptions.Builder().setPopUpTo(R.id.page1Main, false).build()
+)
+}
+
+
+fun storeInput(){
+}
+fun editData(){
+}
+fun loadAllDatas(){
+}
+fun getCardData(){
+}
+
+
+
+
+
+
+
+
+
+
+
